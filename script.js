61ed17dca473a4de221af82c1357f37f2d47f843
kuro-neko.xyz <a.aleman.h@icloud.com>
2025-05-15 18:32:24 -0600

    Make responsive

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let currentSlide = 0;
 
 const main = document.querySelector("main");
+const aboutMeLink = document.querySelector("#about-me-link");
+const project1Link = document.querySelector("#project-1-link");
+const project2Link = document.querySelector("#project-2-link");
+const project3Link = document.querySelector("#project-3-link");
 
 document.addEventListener(
   "wheel",
@@ -17,3 +21,23 @@ document.addEventListener(
   },
   { passive: false }
 );
+
+aboutMeLink.addEventListener("click", () => {
+  currentSlide = 0;
+  main.style.top = `-${window.innerHeight * currentSlide}px`;
+});
+
+project1Link.addEventListener("click", () => {
+  currentSlide = 1;
+  main.style.top = `-${window.innerHeight * currentSlide}px`;
+});
+
+project2Link.addEventListener("click", () => {
+  currentSlide = 2;
+  main.style.top = `-${window.innerHeight * currentSlide}px`;
+});
+
+project3Link.addEventListener("click", () => {
+  currentSlide = 3;
+  main.style.top = `-${window.innerHeight * currentSlide}px`;
+});
